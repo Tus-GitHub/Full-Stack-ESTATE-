dd5c36aed199318924063455de4d9b66c64d8a95
Tus-GitHub <tushardhull22@gmail.com>
2024-06-22 16:46:33 +0530

    complete update user functionality and solved update user api error

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -17,18 +17,18 @@ export const updateUser = async(req, res, next) =>{
             req.body.password =bcryptjs.hashSync(req.body.password, 10);
         }
 
-        const updateUser = await User.findByIdAndUpdate(
+        const updatedUser = await User.findByIdAndUpdate(
             req.params.id, 
             {
             $set:{
                 username: req.body.username,
-                email: req.user.email,
-                password: req.user.password,
-                avatar: req.user.avatar,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
             }
         },{new: true}
     )
-        const {password, ...rest} = updateUser._doc;
+        const {password, ...rest} = updatedUser._doc;
 
         res.status(200).json(rest);
     } catch(error){
